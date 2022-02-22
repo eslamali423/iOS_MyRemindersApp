c2b7b46de1ceb335c42bbfcef808980f6f75fa11
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-22 19:50:32 +0200

    Initial commit

---
+++ MyReminders/MyReminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyReminders
+//
+//  Created by Eslam Ali  on 22/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
