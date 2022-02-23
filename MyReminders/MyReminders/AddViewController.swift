4226ce47bb39933e9105b424bbabf2f949b93a78
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-23 14:32:46 +0200

    create Add Reminders View Controller & set layouts/constrains

--- MyReminders/MyReminders/AddViewController.swift
+++ MyReminders/MyReminders/AddViewController.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var bodyField: UITextField!
+    @IBOutlet weak var titleField: UITextField!
+    
+    public var completion : ((String, String, Date) -> Void)?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bodyField.delegate = self
+        titleField.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSaveButton))
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func didTapSaveButton(){
+        if let title =  titleField.text, !title.isEmpty,
+           let body =  bodyField.text, !body.isEmpty {
+            let date  = datePicker.date
+        
+            self.completion?(title,body,date)
+        }
     }
-    */
 
+ 
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
